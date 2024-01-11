bc6e185076dda405c5e629cb5553980699757ba5
cpbingham <cbingham96@gmail.com>
2024-01-10 19:06:35 -0500

    Adding test for user creation

--- __tests__/users.test.ts
+++ __tests__/users.test.ts
@@ -4,6 +4,7 @@ import { getApp, startApp, cleanUpDatabase, stopApp, getRepository } from "./uti
 import request, { Response } from 'supertest'
 import { EMAIL, USERNAME } from "./utils/testVariables";
 import { commonUserBody } from "./utils/testDataConstructor";
+import { initializeUser } from "./utils/testDataInitializer";
 
 
 const TEST_NAME = 'USER'
@@ -25,18 +26,27 @@ const getUserRepository = (): Repository<User> => {
 }
   
 describe('User Register Test', () => {
-it('should be a sucess to register', async () => {
+    it('should be a success to register', async () => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const response: Response<any> = await request(getApp())
+            .post(`/auth/register`)
+            .send(commonUserBody)
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const response: Response<any> = await request(getApp())
-        .post(`/auth/register`)
-        .send(commonUserBody)
+        expect(response.statusCode).toBe(201)
 
-    expect(response.statusCode).toBe(201)
+        const responseData = response.body.data
+        expect(responseData.username).toBe(USERNAME)
+        expect(responseData.email).toBe(EMAIL)
+        expect(await getUserRepository().count()).toBe(1)
+    })
 
-    const responseData = response.body.data
-    expect(responseData.username).toBe(USERNAME)
-    expect(responseData.email).toBe(EMAIL)
-    expect(await getUserRepository().count()).toBe(1)
-})
+    it('should fail to register with existing email', async () => {
+        await initializeUser()
+        expect(await getUserRepository().count()).toBe(1)
+
+        const response: Response = await request(getApp())
+            .post(`/auth/register`)
+            .send(commonUserBody)
+        expect(response.statusCode).toBe(400)
+    })
 })
--- __tests__/utils/testDataInitializer.ts
+++ __tests__/utils/testDataInitializer.ts
@@ -6,7 +6,9 @@ export const initializeUser = async (): Promise<User> => {
     const body = {
         username: USERNAME,
         email: EMAIL,
-        password: PASSWORD
+        password: PASSWORD,
+        salt: '1234',
+        hash: '1234'
     }
     const user: User = new User()
     Object.assign(user, body)
